2e087bfd74706f414dab86b308cdfe616bf7765a
Nuru Dashdamirli <nurutasdemir@gmail.com>
2022-04-07 14:57:18 +0400

    scale mode "dpi" instead of "font"

--- SIS Renamer/MainForm.Designer.cs
+++ SIS Renamer/MainForm.Designer.cs
@@ -306,8 +306,8 @@
             // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.ClientSize = new System.Drawing.Size(1312, 518);
             this.Controls.Add(this.buttonResetListboxes);
             this.Controls.Add(this.previewNames);
