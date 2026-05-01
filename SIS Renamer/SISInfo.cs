4b236ddd81d30bfbb5594e43c1665c804af95772
Nuru Dashdamirli <49725539+NuruDashdamir@users.noreply.github.com>
2026-05-01 19:32:24 +0400

    refactor: "SISInfo.cs" structure improvements

--- SIS Renamer/SISInfo.cs
+++ SIS Renamer/SISInfo.cs
@@ -17,108 +17,145 @@ namespace SIS_Renamer
 
         public SISInfo(string sisFileLocation, bool fileListIsNeeded = false)
         {
-            //RESET DATA ABOUT PACKAGE
-            appName = "";
-            appVersion = "";
-            supportedDevices = "";
-            appUID = "";
-            vendorName = "";
-            installType = "";
-            applicationType = "";
-            filesInPackage = "";
-
-            //applicationType related boolean flags
-            bool isFlashApp = false, isPyS60_v2_0_0_App = false, isPythonApp = false, isQtApp = false, isTheme = false, isSmartInstaller = false;
-
-
             var sisFile = System.IO.File.Open(sisFileLocation, System.IO.FileMode.Open);
             var sisBinaryReader = new System.IO.BinaryReader(sisFile);
             SISX.SISXFile sis = new SISX.SISXFile(sisBinaryReader);
-            appName = sis.cnt._controller.info.names.fields[0].ToString();
-            appVersion = "v" + sis.cnt._controller.info.version.ToString().Replace(",", ".");
-            vendorName = sis.cnt._controller.info.vendorName.aString.ToString();
-            appUID = "[" + sis.cnt._controller.info.uid.ToString() + "]";
-            isSignedApp = sis.cnt._controller.signatures.Count > 0;
 
-            //detect if package depends on QT (test if app is QT app)
-            foreach (SISX.Fields.SISDependency tmp in sis.cnt._controller.prerequisites.dependencies.fields)
-            {
-                if (tmp.dependencyNames.fields[0].ToString().ToLower() == "qt")
-                {
-                    isQtApp = true;
-                    break;
-                }
-            }
-
-            //set installation type
-            int installTypeID = sis.cnt._controller.info.installType;
-            switch (installTypeID)
-            {
-                case 0: installType = "[SA]"; break;
-                case 1: installType = "[SP]"; break;
-                case 2: installType = "[PU]"; break;
-                case 3: installType = "[PA]"; break;
-                case 4: installType = "[PP]"; break;
-            }
-
-            //set supported devices
-            foreach (SISX.Fields.SISDependency tmp in sis.cnt._controller.prerequisites.targhetDevices.fields)
-            {
-                string tempDeviceUID = tmp.uid.ToString();
-
-                //Change UIDs in supported package list to device type
-                if (tempDeviceUID == "0x101F7961") tempDeviceUID = "[S60v3]";
-                else if (tempDeviceUID == "0x102032BE") tempDeviceUID = "[S60v3FP1]";
-                else if (tempDeviceUID == "0x102752AE") tempDeviceUID = "[S60v3FP2]";
-                else if (tempDeviceUID == "0x1028315F") tempDeviceUID = "[S60v5]";
-                else if (tempDeviceUID == "0x20022E6D") tempDeviceUID = "[S^3]";
-                else if (tempDeviceUID == "0x20032DE7") tempDeviceUID = "[S^4]";
-                else if (tempDeviceUID == "0x2003A678") tempDeviceUID = "[Belle]";
-                else if (tempDeviceUID == "0x101F6300") tempDeviceUID = "[UIQ 3]";
-                else if (tempDeviceUID == "0x101F63DF") tempDeviceUID = "[UIQ 3.1]";
-                else tempDeviceUID = ""; //remove unnamed UIDs
-
-                supportedDevices += tempDeviceUID;
-            }
-            if (supportedDevices == "") supportedDevices = "[NoDevice]";
-
-            //APPLICATION TYPE SECTION
-            //get file names and determine {appType}
-            foreach (SISX.Fields.SISFileDescription tmp in sis.cnt._controller.installBlock.files.fields)
-            {
-                string currentFileLocation = tmp.target.aString.ToString().ToLower();
-
-                if (!isTheme && currentFileLocation.EndsWith(".skn") && currentFileLocation.Contains("private\\10207114\\")) isTheme = true;
-                else if (!isQtApp && currentFileLocation.EndsWith(".qml")) isQtApp = true;
-                else if (!isFlashApp && currentFileLocation.EndsWith(".swf")) isFlashApp = true;
-                else if (!isSmartInstaller && currentFileLocation.Contains(".sis") && currentFileLocation.Contains("private\\2002ccce\\")) isSmartInstaller = true; // .sisx too, detects 99.6% cases
-                else if (!isPyS60_v2_0_0_App && currentFileLocation.EndsWith(".py"))
-                {
-                    isPythonApp = true;
-                    if (currentFileLocation.EndsWith("launcher.py")) isPyS60_v2_0_0_App = true;
-                }
-            }
-
-            //add extradata tags
-            if (isTheme) applicationType += "{THEME}";
-            if (isFlashApp) applicationType += "{FLASH}";
-            if (isQtApp) applicationType += "{QT}";
-            if (isSmartInstaller) applicationType += "{QT-SI}";
-
-            if (isPythonApp)
-            {
-                if (isPyS60_v2_0_0_App) applicationType += "{PyS60 v2.0.0}";
-                else applicationType += "{PyS60 v1.4.5}";
-            }
-
-            //APPLICATION TYPE section end
-            
+            appName = getAppName(sis);
+            appVersion = getAppVersion(sis);
+            supportedDevices = getAppSupportedDevices(sis);
+            appUID = getAppUID(sis);
+            vendorName = getAppVendorName(sis);
+            installType = getAppInstallationType(sis);
+            applicationType = getAppType(sis);
+            isSignedApp = isAppSigned(sis);
+                        
             if (fileListIsNeeded) prepareFileListInPackage(sis);
 
             sisBinaryReader.Close();
             sisFile.Dispose();
             sisFile.Close();
         }
+
+
+        private string getAppName(SISX.SISXFile sis)
+        {
+            return sis.cnt._controller.info.names.fields[0].ToString();
+        }
+
+
+        private string getAppVersion(SISX.SISXFile sis)
+        {
+            return "v" + sis.cnt._controller.info.version.ToString().Replace(",", ".");
+        }
+
+
+        private string getAppVendorName(SISX.SISXFile sis)
+        {
+            return sis.cnt._controller.info.vendorName.aString.ToString();
+        }
+
+
+        private string getAppUID(SISX.SISXFile sis)
+        {
+            return "[" + sis.cnt._controller.info.uid.ToString() + "]";
+        }
+
+
+        private string getAppInstallationType(SISX.SISXFile sis)
+        {
+            switch (sis.cnt._controller.info.installType) // integer installTypeID
+            {
+                case 0: return "[SA]";
+                case 1: return "[SP]";
+                case 2: return "[PU]";
+                case 3: return "[PA]";
+                case 4: return "[PP]";
+                default: return "[UNKNOWN]";
+            }
+        }
+
+
+        private string getAppSupportedDevices(SISX.SISXFile sis)
+        {
+            bool haveOtherUIDs = false;
+
+            System.Text.StringBuilder sb = new System.Text.StringBuilder();
+            foreach (SISX.Fields.SISDependency sisdep in sis.cnt._controller.prerequisites.targhetDevices.fields)
+            {
+                switch (sisdep.uid.ToString())
+                {
+                    case "0x101F7961": sb.Append("[S60v3]"); break;
+                    case "0x102032BE": sb.Append("[S60v3FP1]"); break;
+                    case "0x102752AE": sb.Append("[S60v3FP2]"); break;
+                    case "0x1028315F": sb.Append("[S60v5]"); break;
+                    case "0x20022E6D": sb.Append("[S^3]"); break;
+                    case "0x20032DE7": sb.Append("[S^4]"); break;
+                    case "0x2003A678": sb.Append("[Belle]"); break;
+                    case "0x101F6300": sb.Append("[UIQ 3]"); break;
+                    case "0x101F63DF": sb.Append("[UIQ 3.1]"); break;
+                    default: haveOtherUIDs = true; break; // ignore unknown UID
+                }
+            }
+
+            string result = sb.ToString();
+
+            if (string.IsNullOrEmpty(result))
+            {
+                return haveOtherUIDs ? "[OtherDevice]" : "[NoDevice]";
+            } 
+
+            return result;
+        }
+
+
+        private string getAppType(SISX.SISXFile sis)
+        {
+            // related boolean flags
+            bool isFlashApp = false, isPyS60_v200_App = false, isPythonApp = false, isQtApp = false, isTheme = false, isSmartInstaller = false;
+
+            // detect if package depends on QT
+            foreach (SISX.Fields.SISDependency sisdep in sis.cnt._controller.prerequisites.dependencies.fields)
+            {
+                if (sisdep.dependencyNames.fields[0].ToString().ToLower() == "qt")
+                {
+                    isQtApp = true;
+                    break;
+                }
+            }
+
+            foreach (SISX.Fields.SISFileDescription sisfiledes in sis.cnt._controller.installBlock.files.fields)
+            {
+                string path = sisfiledes.target.aString.ToString().ToLower(); // lowercase
+
+                if (!isTheme && path.EndsWith(".skn") && path.Contains("private\\10207114\\")) isTheme = true;
+                else if (!isQtApp && path.EndsWith(".qml")) isQtApp = true;
+                else if (!isFlashApp && path.EndsWith(".swf")) isFlashApp = true;
+                else if (!isSmartInstaller && path.Contains(".sis") && path.Contains("private\\2002ccce\\")) isSmartInstaller = true; // .sisx too, detects 99.6% cases
+                else if (!isPyS60_v200_App && path.EndsWith(".py"))
+                {
+                    isPythonApp = true;
+                    if (path.EndsWith("launcher.py")) isPyS60_v200_App = true;
+                }
+            }
+
+            System.Text.StringBuilder sb = new System.Text.StringBuilder();
+
+            if (isTheme) sb.Append("{THEME}");
+            if (isFlashApp) sb.Append("{FLASH}");
+            if (isQtApp) sb.Append("{QT}");
+            if (isSmartInstaller) sb.Append("{QT-SI}");
+            if (isPythonApp) sb.Append(isPyS60_v200_App ? "{PyS60 v2.0.0}" : "{PyS60 v1.4.5}");
+
+            return sb.ToString();
+        }
+
+
+        private bool isAppSigned(SISX.SISXFile sis)
+        {
+            return sis.cnt._controller.signatures.Count > 0;
+        }
+
 
         private void prepareFileListInPackage(SISX.SISXFile sis, bool ignoreEmptyTargets = false)
         {
@@ -160,10 +197,12 @@ namespace SIS_Renamer
             filesInPackage = string.Join("\n", fileInfoList.ToArray());
         }
 
+
         public void getData()
         {
             System.Windows.Forms.MessageBox.Show("App Name: " + appName + "\nApp Version: " + appVersion + "\nSupported Devices: " + supportedDevices + "\nUID: " + appUID + "\nVendor: " + vendorName + "\nType: " + installType + "\nExtra: " + applicationType);
             System.Windows.Forms.MessageBox.Show(filesInPackage);
         }
+
     }
 }
